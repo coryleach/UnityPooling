1bc2b35c10c127799094cdbe057255ab5343cc36
cory.leach@gmail.com <cory.leach@gmail.com>
2019-08-28 14:11:13 -0400

    first checkin

---
+++ Runtime/PoolManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gameframe.Pooling
+{
+  [CreateAssetMenu(menuName = "GameJam/Pooling/PoolManager")]
+  public class PoolManager : ScriptableObject
+  {
+    private readonly Dictionary<PoolableGameObject, Pool> poolDictionary = new Dictionary<PoolableGameObject, Pool>();
+
+    public Pool GetPool(PoolableGameObject prefab)
+    {
+      if ( poolDictionary.TryGetValue(prefab,out var pool) )
+      {
+        return pool;
+      }
+
+      pool = new Pool(prefab);
+
+      poolDictionary.Add(prefab, pool);
+
+      return pool;
+    }
+  }
+}
+
---
+++ Runtime/PoolableParticleSystem.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Gameframe.Pooling
+{
+  public class PoolableParticleSystem : PoolableGameObject
+  {
+    [Tooltip("Will include child particle systems which checking if the system is still alive when true")]
+    public bool checkChildren = true;
+    
+    [SerializeField]
+    private ParticleSystem particles;
+    public ParticleSystem Particles => particles;
+
+    private Coroutine coroutine = null;
+    
+    private void OnEnable()
+    {
+      if ( particles == null )
+      {
+        particles = GetComponent<ParticleSystem>();
+      }
+      coroutine = StartCoroutine(WaitForParticleSystem());
+    }
+
+    private void OnDisable()
+    {
+      if (coroutine != null)
+      {
+        Despawn();
+      }
+    }
+
+    private IEnumerator WaitForParticleSystem()
+    {
+      var system = Particles;
+      yield return new WaitUntil( ()=> !system.IsAlive(checkChildren) );
+      coroutine = null;
+      Despawn();
+    }
+  }
+}
